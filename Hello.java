9e50294e3e2a46d6f2437bc0a45a3b94635c2b85
Benjamin Schroeder <btschroeder@email.arizona.edu>
2017-09-11 09:56:53 -0700

    added semicolons

--- Hello.java
+++ Hello.java
@@ -10,24 +10,24 @@ public class Hello {
 
     public static void callSign() {
         
-        System.out.println("Lock S-foils in attack position.")
+        System.out.println("Lock S-foils in attack position.");
 	// Red Leader should add a print statement that says "Lock S-foils in attack position." 
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
-	System.out.println("Stabalize your rear deflectors... watch for enemy fighters.")
+	System.out.println("Stabalize your rear deflectors... watch for enemy fighters.");
 
     }
 
     public static void goldLeader() {
 
         // Gold Leader should add a print statement that says "Loosen up!"
-	System.out.println("Loosen up!")
+	System.out.println("Loosen up!");
     
 
     }
 
     public static void redLeader() {
  	
-	System.out.println("I copy, gold learder!")
+	System.out.println("I copy, gold learder!");
         // Red Leader should add a print statement that says "I copy, gold leader!"
         
 
